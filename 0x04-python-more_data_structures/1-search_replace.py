632a18592188104b3f26cdf12a148dc526506ff9
ZuhairAhmed0 <zuh12390@gmail.com>
2023-01-13 00:31:37 -0800

    replaces all element matching

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    lambdaF = lambda x: int(str.replace(str(x), str(search), str(replace)))
-    return list(map(lambdaF, my_list))
+    s = str(search)
+    r = str(replace)
+    return list(map(lambda x: int(str.replace(str(x), s, r)), my_list))
