2ae6c147e9f5d4a8a8e62e6f76f345011d25dcb8
ZuhairAhmed0 <zuh12390@gmail.com>
2023-01-13 01:36:30 -0800

    multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dicts = {}
-    for x , y in a_dictionary.items():
+    for x, y in a_dictionary.items():
         new_dicts[x] = y * 2
     return new_dicts
-
