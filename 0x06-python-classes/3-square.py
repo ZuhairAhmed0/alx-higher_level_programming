5c1183f1a5f98974aa921ef9ab4327a50a93eba9
ZuhairAhmed0 <zuh12390@gmail.com>
2023-01-26 12:15:28 -0800

    last update

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 2-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """size must be an integer, otherwise raise a TypeError
+    if size is less than 0, raise a ValueError
+    exception with the message size must be >= 0"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not type(self.__size) is int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size**2
