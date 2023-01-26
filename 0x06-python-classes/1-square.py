b7310a2d3687be5dc6e610ab509ada9eec686926
ZuhairAhmed0 <zuh12390@gmail.com>
2023-01-26 11:16:03 -0800

    last update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """Private instance attribute: size"""
+    def __init__(self, size):
+        self.size = size
