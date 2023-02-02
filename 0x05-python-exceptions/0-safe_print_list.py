1c90c8a176043a7fc06fdf1030fb82e3c71fd826
ZuhairAhmed0 <zuh12390@gmail.com>
2023-02-02 09:16:12 -0800

    last uodate

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        size = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            size = size + 1
+    except:
+    return size
