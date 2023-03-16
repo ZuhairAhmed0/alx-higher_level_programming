8ee229b8f97d04a707becb6668c91d6ab309ff0e
ZuhairAhmed0 <zuh12390@gmail.com>
2023-03-16 10:08:54 -0700

    last update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhist` IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhist`
+	IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhist`;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhist`
+	IDENTIFIED BY 'user_0d_1_pwd';
