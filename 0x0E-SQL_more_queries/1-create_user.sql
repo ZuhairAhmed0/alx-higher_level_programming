a9829d4f5e08690ba9a91e5e462a60de616b681d
ZuhairAhmed0 <zuh12390@gmail.com>
2023-03-16 15:39:11 -0700

    last update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,8 @@
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
+
+-- grant all privileges to user user_0d_1
 GRANT ALL PRIVILEGES
    ON *.*
    TO 'user_0d_1'@'localhost';
