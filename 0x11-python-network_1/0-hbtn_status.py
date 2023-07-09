668219c7c86d51f5664b3616ba4689e8d4c766a2
ZuhairAhmed0 <zuh12390@mail.com>
2023-07-09 13:07:11 -0700

    last update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
         res = response.read()
-        
         print("Body response:")
         print("\t- type: {}".format(type(res)))
         print("\t- content: {}".format(res))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ import sys
 import urllib.request
 
 
-
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         value = response.read()
