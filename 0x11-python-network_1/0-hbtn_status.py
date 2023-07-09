d14b10f5ee5f2faad9321e3fea6703424d1483d0
ZuhairAhmed0 <zuh12390@mail.com>
2023-07-09 12:37:09 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         res = response.read()
         
         print("Body response:")
         print("\t- type: {}".format(type(res)))
         print("\t- content: {}".format(res))
-        print("\t- utf8 content: {}".format(res.decode('utf-8')))
+        print("\t- utf8 content: {}".format(res.decode('UTF-8')))
