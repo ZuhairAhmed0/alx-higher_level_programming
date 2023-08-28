df83f8bc7b5e83fa9b3f7080051ca336e945a8ac
ZuhairAhmed0 <zuh12390@gmail.com>
2023-08-28 14:28:48 +0200

    fix error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+""" What's my status? """
 
 if __name__ == "__main__":
     import urllib.request
-    s_url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(s_url) as response:
-        html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf8')))
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
